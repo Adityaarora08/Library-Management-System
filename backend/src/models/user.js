f3e19d9a3a8e7f4d5a7aceab066116724ac1604c
Adityaarora08 <rituishuaddy@gmail.com>
2023-06-17 17:40:27 +0530

    changes

--- backend/src/models/user.js
+++ backend/src/models/user.js
@@ -3,7 +3,7 @@ const { model, Schema } = require("mongoose")
 const UserModel = model(
   "users",
   new Schema({
-    username: { type: String, required: true, unique: true },
+    username: { type: String, required: true},
     password: { type: String, required: true },
     role: { type: String, required: true },
   })
